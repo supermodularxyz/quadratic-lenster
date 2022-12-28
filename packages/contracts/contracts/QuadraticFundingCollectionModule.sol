078dc98519915e1bf7ed1853c124b3de7c0ce2e9
MrDeadCe11 <mister.deadce11@gmail.com>
2022-12-28 11:51:08 -0600

    feat: added a mumbai fork and lend lens fixture in tests.

--- packages/contracts/contracts/QuadraticFundingCollectionModule.sol
+++ packages/contracts/contracts/QuadraticFundingCollectionModule.sol
@@ -1,23 +1,38 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.10;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity >=0.8.10;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
-import {ICollectModule} from './lens/interfaces/ICollectModule.sol';
-import {Errors} from './lens/libraries/Errors.sol';
-import {FeeModuleBase} from './lens//FeeModuleBase.sol';
-import {ModuleBase} from './lens/ModuleBase.sol';
-import {FollowValidationModuleBase} from './lens/FollowValidationModuleBase.sol';
-import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
+// import {ICollectModule} from './lens/interfaces/ICollectModule.sol';
+// import {Errors} from './lens/libraries/Errors.sol';
+// import {FeeModuleBase} from './lens//FeeModuleBase.sol';
+// import {ModuleBase} from './lens/ModuleBase.sol';
+// import {FollowValidationModuleBase} from './lens/FollowValidationModuleBase.sol';
 
 
-contract QuadraticFundingCollectModule {
-    string public greeting;
+// contract QuadraticFundingCollectModule is ICollectModule, FeeModuleBase {
+ 
 
-    constructor(string memory _hub) {
+//     constructor(string memory _hub) {
        
-    }
+//     }
 
-}
+
+   
+//     function initializePublicationCollectModule(
+//         uint256 profileId,
+//         uint256 pubId,
+//         bytes calldata data
+//     ) external returns (bytes memory){
+
+//     }
+
+ 
+//     function processCollect(
+//         uint256 referrerProfileId,
+//         address collector,
+//         uint256 profileId,
+//         uint256 pubId,
+//         bytes calldata data
+//     ) external{}
+// }
--- packages/contracts/test/lensTests/lens.fixture.ts
+++ packages/contracts/test/lensTests/lens.fixture.ts
@@ -1,18 +1,38 @@
+
 import type { SignerWithAddress } from "@nomiclabs/hardhat-ethers/dist/src/signer-with-address";
 //import { lensMumbaiAddresses } from "../utils/constants";
-import { ethers } from "hardhat";
+import { ethers, network } from "hardhat";
 
-import type { Greeter } from "../../types/Greeter";
-import type { Greeter__factory } from "../../types/factories/Greeter__factory";
+import { lensMumbaiAddresses } from "../utils/constants";
+import LensHub from "../../importedABI/LensHub.json"
+import { expect } from "chai";
 
-export async function deployGreeterFixture(): Promise<{ greeter: Greeter }> {
+
+export async function deployLensMumbaiFixture() {
   const signers: SignerWithAddress[] = await ethers.getSigners();
   const admin: SignerWithAddress = signers[0];
+  /* check that api key has been set*/
+  if (!process.env.INFURA_API_KEY)
+  throw new Error("This test requires a mainnet provider set in the INFURA_API_KEY environment variable");
 
-  const greeting: string = "Hello, world!";
-  const greeterFactory: Greeter__factory = <Greeter__factory>await ethers.getContractFactory("Greeter");
-  const greeter: Greeter = <Greeter>await greeterFactory.connect(admin).deploy(greeting);
-  await greeter.deployed();
+    /* Fork mumbai */
+    await network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            blockNumber: 30259508 /* Sep 3rd, 2022 */,
+          },
+        },
+      ],
+    });
 
-  return { greeter };
+  
+  const lensMumbai = new ethers.Contract(lensMumbaiAddresses.LensHubProxy, LensHub.abi, admin)
+  const tx = await lensMumbai.getFollowNFTImpl();
+
+  expect(tx).to.equal("0x1A2BB1bc90AA5716f5Eb85FD1823338BD1b6f772")
+
+  return { lensMumbai };
 }
