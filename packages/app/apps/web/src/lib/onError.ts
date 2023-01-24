c170e96d5a5202e2610618766c8f3ea64a1cdd13
psparacino <tufnelenterprises@gmail.com>
2023-01-24 10:26:43 -0500

    delete app/

--- packages/app/apps/web/src/lib/onError.ts
+++
@@ -1,8 +0,0 @@
-import { ERROR_MESSAGE } from 'data/constants';
-import toast from 'react-hot-toast';
-
-const onError = (error: any) => {
-  toast.error(error?.data?.message ?? error?.message ?? ERROR_MESSAGE);
-};
-
-export default onError;
