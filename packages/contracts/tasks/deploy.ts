d440139cb125001df3a14c830c826d74e7ee9dc5
bitbeckers <code@bitbeckers.com>
2023-01-31 20:29:49 +0100

    chore(review): polishing

--- packages/contracts/tasks/deploy.ts
+++ packages/contracts/tasks/deploy.ts
@@ -2,87 +2,78 @@ import { ContractFactory } from 'ethers';
 import * as fs from 'fs';
 import { task } from 'hardhat/config';
 
-import { lensMumbaiAddresses, lensPolygonAddresses } from './../test/utils/constants';
+import GRANTS_MUMBAI from '../deployments/grants-polygon-mumbai.json';
+import LENS_MUMBAI from '../deployments/lens-polygon-mumbai.json';
+import LENS_POLYGON from '../deployments/lens-polygon.json';
 
 type Contracts = 'QuadraticFundingCurator' | 'QuadraticVoteCollectModule';
 
-task('deploy', 'Deploy contracts and verify')
-  .addOptionalParam('grantsRound', 'The address of the active Grants Round')
-  .addOptionalParam('lensHub', 'The address of the LensHub')
-  .addOptionalParam('moduleGlobals', 'The address of ModuleGlobals')
-  .setAction(async ({ grantsRound, lensHub, moduleGlobals }, { ethers }) => {
-    const [admin] = await ethers.getSigners();
-    let addresses;
+task('deploy', 'Deploy contracts and verify').setAction(async ({}, { ethers }) => {
+  const [admin] = await ethers.getSigners();
+  let addresses;
 
-    if (hre.network.name == 'polygon-mainnet') {
-      addresses = lensPolygonAddresses;
-    } else if (hre.network.name == 'polygon-mumbai') {
-      addresses = lensMumbaiAddresses;
-    } else {
-      addresses = lensPolygonAddresses;
-    }
+  if (hre.network.name == 'polygon-mainnet') {
+    addresses = LENS_POLYGON;
+  } else if (hre.network.name == 'polygon-mumbai') {
+    addresses = LENS_MUMBAI;
+  } else {
+    addresses = LENS_POLYGON;
+  }
 
-    const contracts: Record<Contracts, ContractFactory> = {
-      QuadraticFundingCurator: await ethers.getContractFactory('QuadraticFundingCurator'),
-      QuadraticVoteCollectModule: await ethers.getContractFactory('QuadraticVoteCollectModule'),
-    };
+  const contracts: Record<Contracts, ContractFactory> = {
+    QuadraticFundingCurator: await ethers.getContractFactory('QuadraticFundingCurator'),
+    QuadraticVoteCollectModule: await ethers.getContractFactory('QuadraticVoteCollectModule'),
+  };
 
-    const deployments: Record<Contracts, string> = {
-      QuadraticFundingCurator: '',
-      QuadraticVoteCollectModule: '',
-    };
+  const deployments: Record<Contracts, string> = {
+    QuadraticFundingCurator: '',
+    QuadraticVoteCollectModule: '',
+  };
 
-    const constructorArguments: Record<Contracts, string[]> = {
-      QuadraticFundingCurator: [
-        grantsRound ? grantsRound : '0xCb964E66dD4868e7C71191D3A1353529Ad1ED2F5',
-        admin.address,
-      ],
-      QuadraticVoteCollectModule: [
-        lensHub ? lensHub : addresses.lensHubImplementation,
-        moduleGlobals ? moduleGlobals : addresses.moduleGlobals,
-      ],
-    };
+  const constructorArguments: Record<Contracts, string[]> = {
+    QuadraticFundingCurator: [GRANTS_MUMBAI.GrantsRound, admin.address],
+    QuadraticVoteCollectModule: [addresses.lensHubImplementation, addresses.moduleGlobals],
+  };
 
-    const toFile = (path: string, deployment: Record<Contracts, string>) => {
-      fs.writeFileSync(path, JSON.stringify(deployment), { encoding: 'utf-8' });
-    };
+  const toFile = (path: string, deployment: Record<Contracts, string>) => {
+    fs.writeFileSync(path, JSON.stringify(deployment), { encoding: 'utf-8' });
+  };
 
-    for (const [name, contract] of Object.entries(contracts)) {
-      console.log(`Starting deployment of ${name}`);
-      const factory = contract;
+  for (const [name, contract] of Object.entries(contracts)) {
+    console.log(`Starting deployment of ${name}`);
+    const factory = contract;
 
-      const constructorArgs = Object.entries(constructorArguments).find((entry) => entry[0] === name)?.[1];
-      console.log(`Constructor arguments: ${constructorArgs}`);
+    const constructorArgs = Object.entries(constructorArguments).find((entry) => entry[0] === name)?.[1];
+    console.log(`Constructor arguments: ${constructorArgs}`);
 
-      const instance = constructorArgs ? await factory.deploy(...constructorArgs) : await factory.deploy();
+    const instance = constructorArgs ? await factory.deploy(...constructorArgs) : await factory.deploy();
 
-      await instance.deployed();
+    await instance.deployed();
 
-      console.log(`${name} is deployed to address: ${instance.address}`);
+    console.log(`${name} is deployed to address: ${instance.address}`);
 
-      deployments[name as Contracts] = instance.address;
+    deployments[name as Contracts] = instance.address;
 
-      toFile(`deployments/deployments-${hre.network.name}.json`, deployments);
+    toFile(`deployments/deployments-${hre.network.name}.json`, deployments);
 
-      if (hre.network.name !== ('localhost' || 'hardhat')) {
-        try {
-          const code = await instance.instance?.provider.getCode(instance.address);
-          if (code === '0x') {
-            console.log(`${instance.name} contract deployment has not completed. waiting to verify...`);
-            await instance.instance?.deployed();
-          }
-
-          await hre.run('verify:verify', {
-            address: instance.address,
-            contract: `contracts/${name}.sol:${name}`,
-            constructorArguments: constructorArgs,
-          });
-        } catch ({ message }) {
-          if ((message as string).includes('Reason: Already Verified')) {
-            console.log('Reason: Already Verified');
-          }
-          console.error(message);
+    if (hre.network.name !== ('localhost' || 'hardhat')) {
+      try {
+        const code = await instance.instance?.provider.getCode(instance.address);
+        if (code === '0x') {
+          console.log(`${instance.name} contract deployment has not completed. waiting to verify...`);
+          await instance.instance?.deployed();
         }
+
+        await hre.run('verify:verify', {
+          address: instance.address,
+          constructorArguments: constructorArgs,
+        });
+      } catch ({ message }) {
+        if ((message as string).includes('Reason: Already Verified')) {
+          console.log('Reason: Already Verified');
+        }
+        console.error(message);
       }
     }
-  });
+  }
+});
